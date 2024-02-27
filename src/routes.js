8f262a6b0e75796ab685f6f9f0738bc505a57d7a
Andre Henrique Silva Lima <andrehenriquelima1986@gmail.com>
2024-02-27 11:04:21 -0300

    SPA, useLocations e Hooks

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/Inicio/SobreMim";
+import Menu from "./componentes/Menu";
+
+
+function AppRoutes() {
+  return (
+      <BrowserRouter>
+
+        <Menu />
+
+        
+
+        <Routes>
+          <Route path="/" element={<Inicio/>}/>
+          <Route path="/sobremim" element={<SobreMim/>}/>
+          <Route path="*" element={<div>Página não encontrada</div>}/>
+        </Routes>
+      </BrowserRouter>
+  );
+   
+  
+}
+
+export default AppRoutes;
